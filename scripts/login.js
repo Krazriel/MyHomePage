3a338757a2830c263bdc340491f9af0d0ce568f0
Trung Lam <44829001+Krazriel@users.noreply.github.com>
2022-06-16 11:46:05 -0700

    Add files via upload

---
+++ scripts/login.js
@@ -0,0 +1,54 @@
+var updateDay = setTimeout(getDayOfWeek, 500);
+var updateTime = setTimeout(getTimeOfDay, 500);
+
+function getDayOfWeek(){
+    const weekName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var d = new Date();
+    var day = weekName[d.getDay()];
+    var month = monthName[d.getMonth()];
+    var date = d.getDate();
+
+
+    document.getElementById("dayInfo").innerHTML = day + " " + month + " " + date;
+}
+
+function getTimeOfDay(){
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var period = "AM";
+
+    if(hours > 12){
+        hours = hours - 12;
+        period = "PM";
+    }
+    if(hours == 0){
+        hours = 12;
+    }
+
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    document.getElementById("clockInfo").innerHTML = hours + ":" + minutes + " " + period;
+}
+
+function validatePassword(){
+    var password = document.getElementById("passwordTxt");
+    if(password.value == "Welcome1"){
+        location.assign("https://www.youtube.com/watch?v=iik25wqIuFo");
+    }
+    else{
+        alert("Incorrect");
+    }
+}
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        validatePassword();
+    }
+});
